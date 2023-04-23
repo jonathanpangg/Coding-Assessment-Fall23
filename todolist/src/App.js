0698ab21e03673340574fcfac33f0f725c45a70c
Jonathan Pang <pangj@bu.edu>
2023-04-22 20:41:52 -0400

    init

---
+++ todolist/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <label for="fname">First name:</label> <br></br>
+      <input type="text" id="fname" name="fname"/>
+    </div>
+  );
+}
+
+export default App;
